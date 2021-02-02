360126fa6f5e877d5ba60eebea64beadc10fbb6d
yousuke yamaguchi <yosukey@pharsas.com>
2021-02-02 13:29:00 +0900

    change name

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "山口")
     return "Hello {}!".format(name)
 
 
