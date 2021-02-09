9dcc2196ad4ad6fab95d6ac50339783f6050f03a
yousuke yamaguchi <yosukey@pharsas.com>
2021-02-09 16:40:05 +0900

    fix

--- mysql/sql/init-database.sh
+++ mysql/sql/init-database.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mysql --defaults-extra-file=/etc/mysql/conf.d/my.cnf- test_db < "/docker-entrypoint-initdb.d/001-create-tables.sql"
+mysql --defaults-extra-file=/etc/mysql/conf.d/my.cnf guestbook < "/docker-entrypoint-initdb.d/001-create-table.sql"
 
